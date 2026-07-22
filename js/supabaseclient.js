dd45d5d34bee14ad084d1ea2c7ecf1053743af90
123doctoryt123-web <123doctoryt123@gmail.com>
2026-07-22 17:48:09 +0300

    Update supabaseclient.js

--- js/supabaseclient.js
+++ js/supabaseclient.js
@@ -2,7 +2,7 @@
 // supabaseclient.js — ثوابت مركزية + تهيئة Supabase
 // ============================================================
 window.SUPABASE_URL      = "https://uzsipqgbvgrperzdnqrg.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable__-efdxgVxoLDlKw0FsagYA_bUllFUJl";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6c2lwcWdidmdycGVyemRucXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjE3MTAsImV4cCI6MjA5ODYzNzcxMH0.0ZplujpGR-71R6XZ8wjmnuqWrllNPNx1MxnkW5cSQ5U";
 window.EDGE_FUNCTION_URL = window.SUPABASE_URL + "/functions/v1/smooth-action";
 
 window._supabaseClient = window._supabaseClient || window.supabase.createClient(
